58c7a340f89920e3c993e85f47630f9819194b5d
NhiNguyen1209 <hoangnhi.120900@gmail.com>
2023-05-03 12:38:53 +0700

    nhi-0305-1

--- src/main/java/com/example/DNFrontEnd/Controller/DoctorController.java
+++ src/main/java/com/example/DNFrontEnd/Controller/DoctorController.java
@@ -8,9 +8,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.thymeleaf.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -47,6 +50,36 @@ public class DoctorController {
         model.addAttribute("schedulesResponseList", schedulesResponseList);
 
         return "scheduleDoctor";
+    }
+
+    @PostMapping("schedule")
+    public String getSchedulePost(Model model, HttpServletRequest request, RedirectAttributes redirectAttrs, HttpSession session, @ModelAttribute(name = "medicalDate") String medicalDate) {
+//        private Long doctorId;
+//        private String medicalDate;
+        medicalDate = request.getParameter("medicalDate");
+        redirectAttrs.addFlashAttribute("medicalDate", medicalDate);
+        return "redirect:/doctor/schedule";
+//        String doctorId = session.getAttribute("id").toString();
+//        DateTimeFormatter format1 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+//        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+//        if(!StringUtils.isEmpty(medicalDate)){
+//            LocalDate localDate = LocalDate.parse(medicalDate, format1);
+//            model.addAttribute("medicalDate", format2.format(localDate));
+//            model.addAttribute("medicalDate1", medicalDate);
+//        }else{
+//            LocalDate localDate1 = LocalDate.now();//For reference
+//            medicalDate = localDate1.format(format1);
+//            model.addAttribute("medicalDate", format2.format(localDate1));
+//            model.addAttribute("medicalDate1", medicalDate);
+//        }
+//        ListDoctorScheduleRequest listDoctorScheduleRequest = new ListDoctorScheduleRequest();
+//        listDoctorScheduleRequest.setDoctorId(Long.parseLong(doctorId));
+//        listDoctorScheduleRequest.setMedicalDate(medicalDate);
+//        List<SchedulesResponse> schedulesResponseList = doctorService.getSchedule(listDoctorScheduleRequest, session.getAttribute("token").toString());
+//        System.out.println(schedulesResponseList);
+//        model.addAttribute("schedulesResponseList", schedulesResponseList);
+//
+//        return "scheduleDoctor";
     }
 
 }
